ea20cd709dbeeb67a96993cdf26879ac4308f304
MyHoang <hoangmyit@outlook.com>
2023-05-07 14:15:28 +0700

    fix: cli command in build

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'video-repetition',
+  base: '/video-repetition/',
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
